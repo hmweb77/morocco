fcf77d76e70daa242da03f56a2d2443085894476
hmweb77 <oussamahamdani1@gmail.com>
2025-06-09 17:54:01 +0100

    fixing leaf issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,33 @@
 "use client"
+
+import dynamic from 'next/dynamic';
 import MoroccoWebsiteFooter from "@/components/Footer";
 import LatestBlogSection from "@/components/home/Blogs";
 import MoroccoEbookSection from "@/components/home/CTA";
 import TopExperiencesSection from "@/components/home/Experiences";
 import EnhancedMoroccoHeroSection from "@/components/home/Hero";
-import InteractiveMoroccoMap from "@/components/home/InteractiveMap";
 import PremiumEbooksSection from "@/components/home/SellEbook";
 import TravelersTestimonialsSection from "@/components/home/Testimonials";
 import MoroccoResponsiveNavbar from "@/components/Navbar";
 
+// 🔁 Dynamic import to prevent SSR crash from Leaflet in the map component
+const InteractiveMoroccoMap = dynamic(
+  () => import('@/components/home/InteractiveMap'),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
-   <>
-   <MoroccoResponsiveNavbar></MoroccoResponsiveNavbar>
-   <EnhancedMoroccoHeroSection></EnhancedMoroccoHeroSection>
-   <TopExperiencesSection></TopExperiencesSection>
-   <InteractiveMoroccoMap></InteractiveMoroccoMap>
-   <MoroccoEbookSection></MoroccoEbookSection>
-   <PremiumEbooksSection></PremiumEbooksSection>
-   <TravelersTestimonialsSection></TravelersTestimonialsSection>
-   <LatestBlogSection></LatestBlogSection>
-   <MoroccoWebsiteFooter></MoroccoWebsiteFooter>
-   </>
+    <>
+      <MoroccoResponsiveNavbar />
+      <EnhancedMoroccoHeroSection />
+      <TopExperiencesSection />
+      <InteractiveMoroccoMap />
+      <MoroccoEbookSection />
+      <PremiumEbooksSection />
+      <TravelersTestimonialsSection />
+      <LatestBlogSection />
+      <MoroccoWebsiteFooter />
+    </>
   );
 }
